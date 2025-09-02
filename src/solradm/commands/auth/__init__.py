f7a753a377e7264c99b4546cf5bc27d86fa2af63
jd252387 <jd252387@gmail.com>
2025-09-02 23:07:41 +0300

    refactor: lazily load auth command deps

--- src/solradm/commands/auth/__init__.py
+++ src/solradm/commands/auth/__init__.py
@@ -1,22 +1,28 @@
-import rich
-from rich.pretty import pprint
+"""Manage stored Solr authentication credentials."""
+
 from typer import Typer
 
-from solradm.config import settings, persist
-from solradm.config.interactive import setup_solrauth
+from solradm.config import persist, settings
+from solradm.lazy import lazy_module
+
+setup_solrauth = lazy_module("solradm.config.interactive.setup_solrauth")
 
 app = Typer()
 
 
 @app.command()
 def edit():
-    """Edit stored Solr credentials"""
+    """Edit stored Solr credentials."""
     auth = setup_solrauth.setup()
     settings.set("auth", {"user": auth.login, "password": auth.password})
     persist()
-    rich.print("[success]✅  Updated Solr credentials!")
+    from rich import print as rprint
+
+    rprint("[success]✅  Updated Solr credentials!")
 
 @app.command()
 def view():
-    """View stored Solr credentials"""
+    """View stored Solr credentials."""
+    from rich.pretty import pprint
+
     pprint({"user": settings.auth.user, "password": settings.auth.password})
